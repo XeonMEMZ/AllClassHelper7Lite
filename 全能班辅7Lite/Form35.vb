33f1a78c502df2e9a68a93e0ea1ec1547384d5fd
XeonMEMZ <2716623161@qq.com>
2025-09-14 15:04:35 +0800

    Add files via upload

---
+++ 全能班辅7Lite/Form35.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.InteropServices
+
+Public Class Form35
+    Private Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Private Const HT_CAPTION As Integer = &H2
+    <DllImport("user32.dll")>
+    Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+    End Function
+    <DllImport("user32.dll")>
+    Private Shared Function ReleaseCapture() As Boolean
+    End Function
+
+    Dim speed As Double
+
+    Private Sub Form20_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        If e.Button = MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub RoundedForm_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        Dim path As New GraphicsPath()
+        Dim radius As Integer = 20
+        path.AddArc(0, 0, radius, radius, 180, 90)
+        path.AddArc(Me.Width - radius, 0, radius, radius, 270, 90)
+        path.AddArc(Me.Width - radius, Me.Height - radius, radius, radius, 0, 90)
+        path.AddArc(0, Me.Height - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+        Me.Region = New Region(path)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If speed + 0.1 <= 1 Then
+            speed = speed + 0.1
+            Me.Opacity = speed
+        Else
+            speed = 1
+            Me.Opacity = 1
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If speed - 0.1 >= 0 Then
+            speed = speed - 0.1
+            Me.Opacity = speed
+        Else
+            speed = 0
+            Me.Opacity = 0
+            Me.Close()
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Form35_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim path As New GraphicsPath()
+        Dim radius As Integer = 20
+        path.AddArc(0, 0, radius, radius, 180, 90)
+        path.AddArc(Me.Width - radius, 0, radius, radius, 270, 90)
+        path.AddArc(Me.Width - radius, Me.Height - radius, radius, radius, 0, 90)
+        path.AddArc(0, Me.Height - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+        Me.Region = New Region(path)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer2.Enabled = True
+    End Sub
+End Class
---
+++ 全能班辅7Lite/Module2.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Diagnostics
+
+Module Module2
+    Public Function Exitproc(pName As String) As Boolean
+        Dim processName As String
+        processName = Left(pName, Len(pName) - 4)
+        Dim processes() As Process = Process.GetProcessesByName(processName)
+        If processes.Length > 0 Then
+            For Each proc As Process In processes
+                proc.Dispose()
+            Next
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Module
